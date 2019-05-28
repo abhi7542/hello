f361b9699b32bca72991cb4d3a4218a9d3bf1a9b
abhi7542 <51106824+abhi7542@users.noreply.github.com>
2019-05-27 21:37:39 -0700

    Add files via upload

---
+++ abhi kr.py
@@ -0,0 +1,25 @@
+print('hello world')
+a ='abhishek kumar'
+b =22
+print(b)
+print(a)
+'''yrfrf6yurfwy6rfy'''
+print('heappy learning \n \nwelcom to  python')
+ac=10
+bc=['abhishek','5']
+print(ac,bc)
+a,b,c=20,30,40
+print(a,b,c)
+f=10.45
+print(type(f))
+print(bc)
+bc[1]='prince'
+print(bc)
+print(c>a)
+print(4*567)
+print(40%7)
+
+today='wednesday'
+yoga_day='sunday'
+print(yoga_day is   today)
+print(today)
---
+++ main1.py
@@ -0,0 +1,36 @@
+print('hello wrld')
+
+a= "abhishek"
+print(a)
+
+ab='kumar'
+print(ab)
+print(a+ ab)
+print('this is my first \n python page',end="")
+print(' this is my 2 python page')
+print('hello\n'*10)
+students=['shivam','satyam','sunny','deepak','raju']
+print(students[4])
+students[2]='abhishek'
+print(students)
+print(students[0:3])
+home=['table','chair','bench','microphone','bottle']
+print(home)
+home.append('fan')
+print(home)
+home.insert(2,'fan')
+print(home)
+home.remove('microphone')
+print(home+['pillow','bulb','bed'])
+print(home)
+print(len(home))
+print(max(home))
+print(min(home))
+#tuples
+abhi=['abhi','567']
+print(abhi)
+abhi[0]='kumar'
+print(abhi)
+print(home)
+home.remove("bench")
+print(home)
